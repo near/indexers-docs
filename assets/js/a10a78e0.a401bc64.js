a83902814122d98de64d5f061e98e7f5f4e99edf
Bohdan Khorolets <bogdan@khorolets.com>
2022-04-15 17:37:51 +0300

    Deploy website - based on 7446b435afc4bd54e1d98cf8af1f392a5cd16884

---
+++ assets/js/a10a78e0.a401bc64.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknear_lake_dock=self.webpackChunknear_lake_dock||[]).push([[4826],{2679:function(a){a.exports=JSON.parse('{"updates":{"allTagsPath":"/near-lake-dock/blog/tags","name":"updates","count":7,"permalink":"/near-lake-dock/blog/tags/updates"}}')}}]);
