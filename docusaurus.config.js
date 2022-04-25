d372384124f9869ac934c2eab8c949c78b7f8d56
Bohdan Khorolets <bogdan@khorolets.com>
2022-04-25 12:00:41 +0300

    feat: Link top GitHub link to indexers-docs repo (#3)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/near/indexers-docs/',
             label: 'GitHub',
             position: 'right',
           },
