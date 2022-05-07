6d0a17b6dfdb663dfc16981e5c9944159a050ba4
Bohdan Khorolets <bogdan@khorolets.com>
2022-05-07 15:30:40 +0300

    fix: JS NEAR Lake Framework tutorial missing parts. Add programming language warnings

---
+++ src/components/ProgrammingLanguage/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+import RustLang from '@site/static/img/rust.png';
+import JSLang from '@site/static/img/js.png';
+
+const LANG_MAP = {
+  'rust-lang': 'Rust',
+  'rust': 'Rust',
+  'javascript': 'JavaScript',
+  'js': 'JavaScript',
+  'ts': 'JavaScript',
+  'typescript': 'JavaScript',
+}
+
+function LangImg({ lang }) {
+  switch (lang) {
+    case "rust-lang":
+    case "rust":
+      return <img src={RustLang} />
+    case "javascript":
+    case "js":
+      return <img src={JSLang} />
+    default:
+      return <></>
+  }
+}
+
+export default function ProgrammingLanguage({ lang, small }) {
+  return (
+    <div className={small ? styles.containerSmall : styles.container}>
+      <div className={small ? styles.logoSmall : styles.logo}>
+        <LangImg lang={lang} />
+      </div>
+      {small ?
+        <div className={styles.disclaimer}>
+          {LANG_MAP[lang]}
+        </div>
+      :
+        <div className={styles.disclaimer}>
+        The article is written for {LANG_MAP[lang]} programming language. Concepts, advices, and examples might differ from other programming languages
+        </div>
+      }
+    </div>
+  )
+}
